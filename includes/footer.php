cdb5abfaba7e6e474cc94e90a848b5a6ccfc47e6
lillialover <kuzonmateusz60@gmail.com>
2023-12-01 09:49:06 +0000

    year

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,7 @@
+<?php 
+$year = date("y");   
+?> 
+ 
  <div class="footerContainer">
 <footer>
       <nav>
@@ -8,6 +12,6 @@
           <li><a href="#">Twitter</a></li>
         </menu>
       </nav>
-      <div>&copy;</div>
+      <div>&copy;<?php echo $year;?></div>
     </footer>
 </div>
