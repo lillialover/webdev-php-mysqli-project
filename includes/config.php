908302c71380d020a3489534617dca3868079a97
lillialover <kuzonmateusz60@gmail.com>
2023-12-08 10:53:48 +0000

    db

--- includes/config.php
+++ includes/config.php
@@ -1,9 +1,9 @@
 <?php
 /* Database credentials */
-define("DB_SERVER", "localhost");
-define("DB_USERNAME", "");
+define("DB_SERVER", "127.0.0.1");
+define("DB_USERNAME", "root");
 define("DB_PASSWORD", "");
-define("DB_NAME", "");
+define("DB_NAME", "db1");
  
 /* Attempt to connect to MySQL database */
 $mysqli = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
